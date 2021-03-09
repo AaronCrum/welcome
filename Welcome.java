a1653088cea94c82c28b5873c6794e3b0c5edf68
Aaron <59898602+AaronCrum@users.noreply.github.com>
2021-03-08 22:01:59 -0800

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -1,4 +1,7 @@
-// This program prints Welcome to Java!
+/* 
+ *  Description: This application displays 'Welcome to Java!' in a message dialog box
+*/
+
 public class Welcome {
     public static void main(String[] args) {
         System.out.println("Welcome to Java!"); 
